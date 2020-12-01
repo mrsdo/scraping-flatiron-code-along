4912e6593d03ff866ca928e13b0366ced94b9014
Martinique <me@martidolce.com>
2020-12-01 14:10:07 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,25 @@
+require 'pry'
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+# course.rb
+class Course
 
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
